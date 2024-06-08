597e2370d4f19d66fe39380ed5605137ff46e839
MithicSpirit <rpc01234@gmail.com>
2024-06-08 12:14:47 -0400

    fix more stuff

--- systems/hipparchus/default.nix
+++ systems/hipparchus/default.nix
@@ -167,5 +167,7 @@
     };
     users."mithic" = import ./home/mithic.nix;
   };
+  systemd.services."user@1000".environment = builtins.mapAttrs (_: toString)
+    config.home-manager.users."mithic".home.sessionVariables;
 
 }
