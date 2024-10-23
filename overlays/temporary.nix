c2a316380262fc54b69763c31223126d6f636532
MithicSpirit <rpc01234@gmail.com>
2024-10-23 18:55:33 -0400

    fix more stuff

--- overlays/temporary.nix
+++ overlays/temporary.nix
@@ -13,4 +13,45 @@ final: prev: {
     ];
   });
 
+  sway-unwrapped = prev.sway-unwrapped.overrideAttrs (
+    finalAttrs: prevAttrs: {
+      version = "1.10-rc4";
+      src = prevAttrs.src.override {
+        hash = "sha256-O8zpOZ7ttPQv8xoH3ytwj2x/evw3+ghHsELEzVh19Q8=";
+      };
+
+      buildInputs =
+        with final;
+        [
+          libGL
+          wayland
+          libxkbcommon
+          pcre2
+          json_c
+          libevdev
+          pango
+          cairo
+          libinput
+          gdk-pixbuf
+          librsvg
+          wayland-protocols
+          libdrm
+          (final.wlroots_0_18.override { inherit (finalAttrs) enableXWayland; })
+        ]
+        ++ lib.optionals finalAttrs.enableXWayland [
+          xorg.xcbutilwm
+        ];
+
+      mesonFlags =
+        let
+          inherit (final.lib.strings) mesonEnable mesonOption;
+          sd-bus-provider = if finalAttrs.systemdSupport then "libsystemd" else "basu";
+        in
+        [
+          (mesonOption "sd-bus-provider" sd-bus-provider)
+          (mesonEnable "tray" finalAttrs.trayEnabled)
+        ];
+    }
+  );
+
 }
