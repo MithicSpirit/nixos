863e75a744877dbd0d1112166af73ffd07a7798b
MithicSpirit <rpc01234@gmail.com>
2024-10-31 16:40:22 -0400

    fix more stuff

--- overlays/temporary.nix
+++ overlays/temporary.nix
@@ -1,18 +1,5 @@
 final: prev: {
 
-  tlp = prev.tlp.overrideAttrs (old: {
-    postPatch = ''
-      substituteInPlace Makefile --replace-fail ' ?= /usr/' ' ?= /'
-    '';
-    makeFlags = [
-      "TLP_NO_INIT=1"
-      "TLP_WITH_ELOGIND=0"
-      "TLP_WITH_SYSTEMD=1"
-
-      "DESTDIR=${placeholder "out"}"
-    ];
-  });
-
   sway-unwrapped =
     (prev.sway-unwrapped.override { wlroots = final.wlroots_0_18; }).overrideAttrs
       (
@@ -21,7 +8,6 @@ final: prev: {
           src = prevAttrs.src.override {
             hash = "sha256-PzeU/niUdqI6sf2TCG19G2vNgAZJE5JCyoTwtO9uFTk=";
           };
-
           mesonFlags =
             let
               inherit (final.lib.strings) mesonEnable mesonOption;
