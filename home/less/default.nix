de8193ed887c5479e85ee0241d4cd38d008e05e9
MithicSpirit <rpc01234@gmail.com>
2024-12-15 18:36:32 -0500

    fix more stuff

--- home/less/default.nix
+++ home/less/default.nix
@@ -3,11 +3,16 @@
 
   programs.less.enable = true;
 
-  home.sessionVariables = {
-    "LESS" =
-      "--RAW-CONTROL-CHARS --use-color -i -Dd+m-\\$-Dk+r-\\$-Ds+y-\\$-Du+b-\\$";
-    "LESSHISTFILE" = "${config.xdg.cacheHome}/less/history";
-    "LESSUTFCHARDEF" = "E000-F8FF:p,F0000-FFFFD:p,100000-10FFFD:p";
-  };
+  home.sessionVariables =
+    let
+      self = config.home.sessionVariables;
+    in
+    {
+      "LESS" =
+        "--RAW-CONTROL-CHARS --use-color -i -Dd+m-\\$-Dk+r-\\$-Ds+y-\\$-Du+b-\\$";
+      "LESSHISTFILE" = "${config.xdg.cacheHome}/less/history";
+      "LESSUTFCHARDEF" = "E000-F8FF:p,F0000-FFFFD:p,100000-10FFFD:p";
+      "SYSTEMD_LESS" = self."LESS";
+    };
 
 }
