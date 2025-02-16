dfe91d9d0a04851bc0ddedf05a5a9928ca916f36
MithicSpirit <rpc01234@gmail.com>
2025-02-16 13:19:01 -0500

    fix more stuff

--- host/zswap/default.nix
+++ host/zswap/default.nix
@@ -8,7 +8,7 @@
   ];
 
   boot.kernel.sysctl = {
-    "vm.swappiness" = 80;
+    "vm.swappiness" = 120;
     "vm.watermark_boost_factor" = 0;
     "vm.watermark_scale_factor" = 125;
     "vm.page-cluster" = 0;
