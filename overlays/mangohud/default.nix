bf22130ba67dd8a11d654ecd7fe3a037d07e94af
MithicSpirit <rpc01234@gmail.com>
2025-06-02 14:03:50 -0400

    fix more stuff

--- overlays/mangohud/default.nix
+++ overlays/mangohud/default.nix
@@ -8,6 +8,12 @@ final: prev: {
       fetchSubmodules = true;
       hash = "sha256-RsQxvl4C2neKBnltNf2rBPpcyDwW+V7V1qXFF4cryRY=";
     };
-    patches = prevAttrs.patches ++ [ ./wayland_keybinds.diff ];
+    patches = (prevAttrs.patches or [ ]) ++ [
+      (final.fetchpatch {
+        name = "fix-wayland-keybinds";
+        url = "https://github.com/flightlessmango/MangoHud/commit/ccf973659e5b7d5cfc1b67635bcc03c0abf0f9d2.diff";
+        hash = "sha256-gk1Ix0hndhLr+2ZB0k9Oq07pE18LQJxLH2TQ9k2bmcA=";
+      })
+    ];
   });
 }
--- overlays/zathura/default.nix
+++ overlays/zathura/default.nix
@@ -1,9 +1,22 @@
-_final: prev: {
+final: prev: {
   zathuraPkgs = prev.zathuraPkgs.overrideScope (
     _finalScope: prevScope: {
 
       zathura_core = prevScope.zathura_core.overrideAttrs (prevAttrs: {
-        patches = (prevAttrs.patches or [ ]) ++ [ ./recolor_alpha.diff ];
+        version = "0.5.11-r3116.g69c9e6c";
+        src = final.fetchFromGitHub {
+          owner = "pwmt";
+          repo = "zathura";
+          rev = "69c9e6c5e31e00756bd8358eee79c4eef5ebc3e5";
+          hash = "sha256-/6VS2fNllDhh4kU68FdKt/tYWRvq4ebMVdb0GDGxO28=";
+        };
+        patches = (prevAttrs.patches or [ ]) ++ [
+          (final.fetchpatch {
+            name = "recolor-lightness";
+            url = "https://github.com/pwmt/zathura/commit/a3c8dc86c94a85fcec3ba3d4f1f7dc393217b7d8.diff";
+            hash = "sha256-wSZ0Z4QSfomT87i+Ya0Jh50ze7xht5Uj7nRM7TuZBSY=";
+          })
+        ];
       });
 
     }
