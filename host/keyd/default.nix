b3d49d13f34fd531540eb7eab723a0048f53dcd1
MithicSpirit <rpc01234@gmail.com>
2025-07-28 18:41:49 -0400

    fix more stuff

--- host/keyd/default.nix
+++ host/keyd/default.nix
@@ -1,7 +1,9 @@
 { ... }:
 {
+
   services.keyd = {
     enable = true;
+
     keyboards.default = {
       ids = [ "*" ];
       settings.main = {
@@ -20,4 +22,12 @@
       };
     };
   };
+
+  environment.etc."libinput/local-overrides.quirks".text = ''
+    [Serial Keyboards]
+    MatchUdevType=keyboard
+    MatchName=keyd virtual keyboard
+    AttrKeyboardIntegration=internal
+  '';
+
 }
