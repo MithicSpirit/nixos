db21a51ef6bbca538f625d2f0a362a93d8523b08
MithicSpirit <rpc01234@gmail.com>
2026-02-14 17:10:01 -0500

    misc: fix wait-for-internet
    Do not use https.  This is just checking whether we have an internet
    connection, not whether the certificates are working.

--- pkgs/wait-for-internet/default.nix
+++ pkgs/wait-for-internet/default.nix
@@ -3,10 +3,10 @@
   writeShellScript,
   curl,
   coreutils,
-  url ? "https://example.com",
+  url ? "http://example.com",
 }:
 writeShellScript "wait-for-internet" ''
-  while ! '${lib.getExe curl}' -fsSLm 10 --connect-timeout 2 -o /dev/null '${url}'
+  while ! '${lib.getExe curl}' -fsLm 10 --connect-timeout 2 -o /dev/null '${url}'
   do '${lib.getExe' coreutils "sleep"}' 1
   done
 ''
